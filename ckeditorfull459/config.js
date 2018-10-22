80f510d1e26b0324d7f80d32ef15981b140effa1
Sergnio <sergnio@gmail.com>
2018-10-22 17:22:26 -0500

    Disallow automatic modifying of html code within the editors

--- ckeditorfull459/config.js
+++ ckeditorfull459/config.js
@@ -7,6 +7,9 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	
+
+	// allowedContent = true - disables automatic filtering and organizing of html code, necessary
+	// if colgroups need to be used
+    config.allowedContent = true;
 	config.extraAllowedContent = 'table[class]; td(subhead); span(score); th[class];';
 };
